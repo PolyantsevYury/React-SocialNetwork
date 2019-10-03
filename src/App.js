41ac0532d3b7d55853e2c896bc0ed924e28ef22e
Yuri-codes <52564127+Yuri-codes@users.noreply.github.com>
2019-10-03 21:33:33 +0400

    53-classComponents

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 const App = (props) => {
     return (
@@ -19,6 +20,8 @@ const App = (props) => {
                        render={() => <DialogsContainer store={props.store} /> }/>
                 <Route path="/profile"
                        render={ () => <Profile store={props.store} /> }/>
+               <Route path='/users'
+                      render={ () => <UsersContainer />}/>
                 <Route path="/news" component={News}/>
                 <Route path="/music" component={Music}/>
                 <Route path="/settings" component={Settings}/>
--- src/Redux/dialogs-reducer.js
+++ src/Redux/dialogs-reducer.js
@@ -21,28 +21,26 @@ let initialState = {
 };
 
 const dialogsReducer = (state = initialState, action) => {
+
     switch (action.type) {
-        case ADD_MESSAGE: {
-            let newMessage = {
-                id: 1,
-                message: state.newMessageText
+        case ADD_MESSAGE:
+            let newMessage = state.newMessageText;
+            return {
+                ...state,
+                newMessageText: '',
+                messages: [...state.messages, {id: 1, message: newMessage}]
             };
-            let stateCopy = {...state};
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = '';
-            return stateCopy;
-        }
-        case UPDATE_NEW_MESSAGE_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newMessageText = action.newText;
-            return stateCopy;
-        }
+
+        case UPDATE_NEW_MESSAGE_TEXT:
+            return {
+                ...state,
+                newMessageText: action.newText
+            };
+
         default:
             return state;
     }
 };
-
 
 export const addMessageActionCreator = () => ({ type: ADD_MESSAGE });
 export const updateNewMessageTextActionCreator = (text) =>
