ef204223cdbabaeb9e8856db9363ef8563f19d0c
Yuri-codes <52564127+Yuri-codes@users.noreply.github.com>
2019-10-09 18:26:40 +0400

    60-withRouter

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -8,7 +8,7 @@ let Users = (props) => {
     let pagesCount = Math.ceil( props.totalUsersCount / props.pageSize);
 
     let pages = [];
-    for (let i=1; i <= pagesCount && i < 10; i++) {
+    for (let i=1; i <= pagesCount /*&& i < 10*/; i++) {
         pages.push(i);
     }
 
